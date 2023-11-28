9e5aa60f16b49d15ef4aae5d50280c3dba7315aa
Dome1992 <forlanodomenico92@mail.com>
2023-11-28 19:49:10 +0100

    feat- ciclo for

--- script.js
+++ script.js
@@ -7,12 +7,48 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro e
 const gridElement = document.getElementById("grid")
 
 
+    
+
+
 // ciclo for per creare 100 elementi
-for (let i = o; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
-    // creare nuovo elemento ("div","square")
-    const nuovoElemento = creaElemento("div", "square");
+    // creare nuovo elemento ("div")
+    const newElement = document.createElement("div");
 
-    // agganciare con APPEND  il nuovo elemento creato all'id "GRID"
-    gridElement.append(nuovoElemento);
+    // creare nuovo elemento ("class=square")
+    newElement.classList.add("square");
+
+    // creare elemento (SPAN) con il numero corrente assegnando (i)
+    const numeroElement = document.createElement("span");
+    numeroElement.textContent = i;
+
+    // creare nuovo elemento "myBtn"
+    const customButtonElement = document.createElement("myBtn");
+
+    // Event listener per click all'evento e console log per stampare risultato
+    customButtonElement.addEventListener("click", function () {
+    console.log("Hai cliccato sulla casella" + i);
+    });
+ 
+    // aggianciare con APPEND  il nuovo elemento creato (numeroElemnt)  a (newElement che sarebbe square)
+    newElement.appendChild(numeroElement);
+
+    // agganciare con APPEND  il nuovo elemento creato "newElement" all'id "GRID"
+    gridElement.append(newElement);
+
+    // agganciare con APPEND il nuovo elemento creato (buttonElement) a (newElement che sarebbe sqaure)
+    newElement.appendChild(customButtonElement);
 }
+
+
+/* creare nuovo elemento ("myBtn")
+const buttonElement = document.createElement("myBtn");*/
+
+/* agganciare con APPEND il nuovo elemento creato (buttonElement) a (newElement che sarebbe sqaure)
+buttonElement.appendChild(newElement)*/
+
+/* Event listener per click all'evento e console log per stampare risultato
+buttonElement.addEventListener("click", function () {
+ console.log("ciao mamma");
+});*/
