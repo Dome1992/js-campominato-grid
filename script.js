25a1c8384b95b9538ccf482f70a8b2ffa41f4c98
Dome1992 <forlanodomenico92@mail.com>
2023-11-29 11:38:42 +0100

    feat- prima parte Campo Minato

--- script.js
+++ script.js
@@ -3,52 +3,49 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
-// selezione dell'elemento contenitore
-const gridElement = document.getElementById("grid")
+// Seleziona l'elemento pulsante "generaGriglia" in Html
+const generaGrigliaButton = document.getElementById("generaGriglia");
+
+// Aggiungi un event listener per il clic sul pulsante "generaGriglia"
+generaGrigliaButton.addEventListener("click",generaGriglia )
 
 
+
+// Funzione per generare la griglia
+function generaGriglia() {
+   
+    // selezione dell'elemento contenitore
+    const gridElement = document.getElementById("grid")
+
+    // ciclo for per creare 100 celle
+    for (let i = 1; i <= 100; i++) {
+
+        // creare nuovo elemento ("div")
+        const newElement = document.createElement("div");
+
+        // creare nuovo elemento ("class=square")
+        newElement.classList.add("square");
+
+        // Appendere a newElement ovvero square variabile i, ovvero il numero della cella 
+        newElement.append(i);
+
+        // Event listener per click all'evento e console log per stampare risultato
+        newElement.addEventListener("click", 
+            function () {
+
+                // Creare nuova classe per il click sulle square
+                this.classList.add("clicked");
+                console.log("Hai cliccato sulla casella" + i);
+            }
+        );
     
+        // Appendere il nuovo elemento creato "newElement" all'id "GRID"
+        gridElement.append(newElement);
+
+        
+    }
 
 
-// ciclo for per creare 100 elementi
-for (let i = 1; i <= 100; i++) {
 
-    // creare nuovo elemento ("div")
-    const newElement = document.createElement("div");
-
-    // creare nuovo elemento ("class=square")
-    newElement.classList.add("square");
-
-    // creare elemento (SPAN) con il numero corrente assegnando (i)
-    const numeroElement = document.createElement("span");
-    numeroElement.textContent = i;
-
-    // creare nuovo elemento "myBtn"
-    const customButtonElement = document.createElement("myBtn");
-
-    // Event listener per click all'evento e console log per stampare risultato
-    customButtonElement.addEventListener("click", function () {
-    console.log("Hai cliccato sulla casella" + i);
-    });
- 
-    // aggianciare con APPEND  il nuovo elemento creato (numeroElemnt)  a (newElement che sarebbe square)
-    newElement.appendChild(numeroElement);
-
-    // agganciare con APPEND  il nuovo elemento creato "newElement" all'id "GRID"
-    gridElement.append(newElement);
-
-    // agganciare con APPEND il nuovo elemento creato (buttonElement) a (newElement che sarebbe sqaure)
-    newElement.appendChild(customButtonElement);
 }
-
-
-/* creare nuovo elemento ("myBtn")
-const buttonElement = document.createElement("myBtn");*/
-
-/* agganciare con APPEND il nuovo elemento creato (buttonElement) a (newElement che sarebbe sqaure)
-buttonElement.appendChild(newElement)*/
-
-/* Event listener per click all'evento e console log per stampare risultato
-buttonElement.addEventListener("click", function () {
- console.log("ciao mamma");
-});*/
+    
